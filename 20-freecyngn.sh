1453ffac88cabda443afea2e70d817f4884d2510
Micha-Btz <github@mdomann.tk>
2017-09-06 20:13:33 +0200

    cleanup

--- 20-freecyngn.sh
+++ 20-freecyngn.sh
@@ -1,5 +1,5 @@
 #!/sbin/sh
-#
+set -x
 # /system/addon.d/20-freecyngn.sh
 # During a CyanogenMod upgrade this script repatches CyanogenMod
 # using the freecyngn patchset.
@@ -12,34 +12,23 @@ deleteApk() {
 del_files() {
 cat <<EOF
 BasicDreams
-Browser
 privacy-browser
 CaptivePortalLogin
 CMAccount
 CMFileManager
-CMS
-CMSetupWizard
 CMWallpapers
 CyanogenSetupWizard
 Email
 Exchange2
-HoloSpiral
 HoloSpiralWallpaper
 LivePicker
 LiveWallpapers
 LiveWallpapersPicker
-MagicSmoke
-MusicVisualization
 NoiseField
 PhaseBeam
 PhotoPhase
 PhotoTable
-SetupWizard
-TimeService
 VisualizationWallpapers
-Voice+
-VoiceDialer
-VoicePlus
 EOF
 }
 
@@ -50,9 +39,9 @@ if [[ "$1" == "post-restore" ]] || [[ "$1" == "" ]]; then
 fi
 
 # remove guest user - make sure you deleted all guest user before
-mount -o rw,remount,rw /system
+#mount -o rw,remount,rw /system
 cp /system/build.prop /system/build.prop.old
 echo "fw.max_users=1" >> /system/build.prop
 echo "fw.show_multiuserui=0" >> /system/build.prop
-mount -o ro,remount,ro /system
+#mount -o ro,remount,ro /system
 
