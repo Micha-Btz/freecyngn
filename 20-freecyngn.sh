f4bf7fc3de2d1d6552382070cc65213f12143b13
Micha-Btz <github@mdomann.tk>
2017-09-10 17:26:05 +0200

    remove CMACount andf fix build prob insert

--- 20-freecyngn.sh
+++ 20-freecyngn.sh
@@ -14,7 +14,6 @@ cat <<EOF
 BasicDreams
 privacy-browser
 CaptivePortalLogin
-CMAccount
 CMFileManager
 CMWallpapers
 CyanogenSetupWizard
@@ -39,11 +38,12 @@ if [[ "$1" == "post-restore" ]] || [[ "$1" == "" ]]; then
 fi
 
 # remove guest user - make sure you deleted all guest user before
-if ( ! grep -q "fw.max_users=1" /system/build.prop ); then
-  echo "max users already set"
-            exit 1
+if ( ! grep -q "fw.max_users=1" /system/build.prop );
+        then
+        echo "max users already set"
+        exit 1;
     else
-    cp /system/build.prop /system/build.prop.old
-    echo "fw.max_users=1" >> /system/build.prop
-    echo "fw.show_multiuserui=0" >> /system/build.prop
+    cp /system/build.prop /system/build.prop.old;
+    echo "fw.max_users=1" >> /system/build.prop;
+    echo "fw.show_multiuserui=0" >> /system/build.prop;
 fi
