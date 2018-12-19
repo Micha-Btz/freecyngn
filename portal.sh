234d58bbdc9022b93105d24fc34f358628cae30a
Micha-Btz <github@mdomann.tk>
2018-12-19 22:06:38 +0100

    add time server to portal.sh

--- portal.sh
+++ portal.sh
@@ -1,6 +1,10 @@
 #!/sbin/sh
-#
+
+#disable captive portal login
 pm disable com.android.captiveportallogin
 settings put global captive_portal_detection_enabled 0
 settings put global captive_portal_server localhost
 settings put global captive_portal_mode 0
+
+# Disable Global NTP Server
+settings put global ntp_server 127.0.0.1
