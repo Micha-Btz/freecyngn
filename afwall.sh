828877e6b55ddfed6323175644bdf254a2780db0
Micha-Btz <github@mdomann.tk>
2019-05-26 11:50:36 +0200

    restructure

--- afwall.sh
+++ afwall.sh
@@ -13,10 +13,6 @@ echo 1 > /proc/sys/net/ipv6/conf/default/disable_ipv6
 echo 2 > /proc/sys/net/ipv6/conf/all/use_tempaddr
 echo 2 > /proc/sys/net/ipv6/conf/default/use_tempaddr
 
-## System
-# Disable Global NTP Server
-settings put global ntp_server 127.0.0.1
-
 ####################
 # iptables #
 ####################
@@ -43,6 +39,11 @@ $IP6TABLES -P OUTPUT DROP
 $IPTABLES -A INPUT -i lo -j ACCEPT
 $IPTABLES -A "afwall" -o lo -j ACCEPT
 
+#####################
+# Incoming Traffic #
+#####################
+# Allow all traffic from an established connection
+$IPTABLES -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
 # Set a specific DNS-Server (dismail.de AdBlocking DNS-Server) for all networks except home WiFi (192.168.2.0/24)
 $IPTABLES -t nat -I OUTPUT ! -s 192.168.2.0/24 -p tcp --dport 53 -j DNAT --to-destination 80.241.218.68:53
 $IPTABLES -t nat -I OUTPUT ! -s 192.168.2.0/24 -p udp --dport 53 -j DNAT --to-destination 80.241.218.68:53
@@ -52,29 +53,45 @@ $IPTABLES -t nat -A OUTPUT -p tcp --dport 123 -j DNAT --to-destination 131.188.3
 $IPTABLES -t nat -A OUTPUT -p udp --dport 123 -j DNAT --to-destination 131.188.3.222:123
 
 #####################
-# Incoming Traffic #
-#####################
-# Allow all traffic from an established connection
-$IPTABLES -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
-
-# Alle Pakete ordentlich zurückweisen
-$IPTABLES -A INPUT -p tcp -j REJECT --reject-with tcp-reset
-$IPTABLES -A INPUT -j REJECT --reject-with icmp-port-unreachable
-
-#####################
 # home rules        #
 #####################
-# Allow SSH from desktop
-$IPTABLES -A INPUT -p tcp --dport 22 -s 192.168.2.105 -j ACCEPT
-#dietpi
-$IPTABLES -A OUTPUT -d 192.168.2.107 -j ACCEPT
+export SYSIPHUS=192.168.2.105
+export SCHNUDDEL=192.168.2.106
+export DIETPI=192.168.2.107
+export BQ=192.168.2.108
+export Z1C=192.168.2.109
+export CEILING=192.168.2.150
+
+$IPTABLES -A OUTPUT -s ${BQ} -d ${DIETPI} -j ACCEPT
+$IPTABLES -A OUTPUT -s ${BQ} -d ${SYSIPHUS} -j ACCEPT
+$IPTABLES -A OUTPUT -s ${Z1C} -d ${SYSIPHUS} -j ACCEPT
+$IPTABLES -A OUTPUT -s ${Z1C} -d ${DIETPI} -j ACCEPT
+$IPTABLES -A OUTPUT -s ${BQ} -d ${CEILING} -j ACCEPT
+$IPTABLES -A OUTPUT -s ${Z1C} -d ${CEILING} -j ACCEPT
+$IPTABLES -A OUTPUT -s ${BQ} -d ${SCHNUDDEL} -j ACCEPT
+$IPTABLES -A OUTPUT -s ${Z1C} -d ${SCHNUDDEL} -j ACCEPT
+$IPTABLES -A INPUT -s ${SYSIPHUS} -d ${BQ} -j ACCEPT
+$IPTABLES -A INPUT -s ${SYSIPHUS} -d ${Z1C} -j ACCEPT
+$IPTABLES -A INPUT -s ${DIETPI} -d ${Z1C} -j ACCEPT
+$IPTABLES -A INPUT -s ${DIETPI} -d ${BQ} -j ACCEPT
+$IPTABLES -A INPUT -s ${CEILING} -d ${BQ} -j ACCEPT
+$IPTABLES -A INPUT -s ${CEILING} -d ${Z1C} -j ACCEPT
+$IPTABLES -A INPUT -s ${SCHNUDDEL} -d ${BQ} -j ACCEPT
+$IPTABLES -A INPUT -s ${SCHNUDDEL} -d ${Z1C} -j ACCEPT
+
+
 #yeelight multi cast address
-#i#$IPTABLES -A OUTPUT -d 192.168.2.150 -j ACCEPT
 #export CHROMECAST_IP=192.168.2.150 # Adjust to the Chromecast IP in your local network
-#iptables -A INPUT -s ${CHROMECAST_IP}/32 -p udp -m multiport --sports 32768:61000 -m multiport --dports 32768:61000 -m comment --comment "Allow Chromecast UDP data (inbound)" -j ACCEPT
-#iptables -A OUTPUT -d ${CHROMECAST_IP}/32 -p udp -m multiport --sports 32768:61000 -m multiport --dports 32768:61000 -m comment --comment "Allow Chromecast UDP data (outbound)" -j ACCEPT
-#iptables -A OUTPUT -d ${CHROMECAST_IP}/32 -p tcp -m multiport --dports 8008:8009 -m comment --comment "Allow Chromecast TCP data (outbound)" -j ACCEPT
-#iptables -A OUTPUT -d 239.255.255.250/32 -p udp --dport 1982 -m comment --comment "Allow Chromecast SSDP" -j ACCEPT
-#iptables -A INPUT -d 239.255.255.250/32 -p udp --dport 1982 -m comment --comment "Allow Chromecast SSDP" -j ACCEPT
-#iptables -I INPUT -p udp -m udp --dport 32768:61000 -j ACCEPT
-#$IPTABLES -A INPUT -s 192.168.2.150 -j ACCEPT
+#$IPTABLES -A INPUT -s ${CEILING} -p udp -m multiport --sports 32768:61000 -m multiport --dports 32768:61000 -m comment --comment "Allow Chromecast UDP data (inbound)" -j ACCEPT
+#$IPTABLES -A OUTPUT -d ${CEILING} -p udp -m multiport --sports 32768:61000 -m multiport --dports 32768:61000 -m comment --comment "Allow Chromecast UDP data (outbound)" -j ACCEPT
+#$IPTABLES -A OUTPUT -d ${CEILING} -p tcp -m multiport --dports 8008:8009 -m comment --comment "Allow Chromecast TCP data (outbound)" -j ACCEPT
+#$IPTABLES -A OUTPUT -d 239.255.255.250/32 -p udp --dport 1982 -m comment --comment "Allow Chromecast SSDP" -j ACCEPT
+#$IPTABLES -A INPUT -d 239.255.255.250/32 -p udp --dport 1982 -m comment --comment "Allow Chromecast SSDP" -j ACCEPT
+#$IPTABLES -I INPUT -p udp -m udp --dport 32768:61000 -j ACCEPT
+
+$IPTABLES -A OUTPUT -d 192.168.2.0/24 -j ACCEPT
+$IPTABLES -A OUTPUT -d 239.255.255.250/32 -p udp --dport 1982 -j ACCEPT
+
+# Alle Pakete ordentlich zurückweisen
+#$IPTABLES -A INPUT -p tcp -j REJECT --reject-with tcp-reset
+#$IPTABLES -A INPUT -j REJECT --reject-with icmp-port-unreachable
